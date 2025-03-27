5aaed6a376702c5d30097dda5012f069857d10b2
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2025-03-27 18:05:40 +0100

    Scaffolding (1)

---
+++ src/test/java/org/fikua/issuer/IssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fikua.issuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
